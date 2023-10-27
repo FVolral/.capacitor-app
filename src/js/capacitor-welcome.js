ac4a5ce0ee8ffca26e7afa27c3179fe977ff177e
Volral François <fvolral@awegroup.be>
2023-10-27 11:56:44 +0200

    répart la repro afin de tester avec des images croppée de paillettes mais ça ne fonctionne quand même pas

--- src/js/capacitor-welcome.js
+++ src/js/capacitor-welcome.js
@@ -1,5 +1,5 @@
 import { SplashScreen } from "@capacitor/splash-screen";
-
+import { FilePicker } from '@capawesome/capacitor-file-picker';
 import {
   BarcodeScanner,
 } from '@capacitor-mlkit/barcode-scanning';
@@ -15,29 +15,30 @@ window.customElements.define(
 
       const root = this.attachShadow({ mode: "open" });
       root.innerHTML = `
-    <main>
-      <h1>Capacitor Reproduction</h1>
-      <div class="barCode1">
-      </div>
-      <div class="barCode2">
-      </div>
-      <button id="open-browser">Open Browser</button>
-    </main>
-    `;
+        <main></main>
+      `;
+    }
+
+    async readBarcodeFromImage () {
+      const { files } = await FilePicker.pickImages({ multiple: false });
+      const path = files[0].path
+
+      if (!path) {
+        console.error('no path')
+        return
+      }
+
+      const { barcodes } = await BarcodeScanner.readBarcodesFromImage({
+        path,
+        formats: ['CODE_128']
+      });
+      alert(`barcodes: ${JSON.stringify(barcodes)}`)
     }
 
     async connectedCallback() {
-      const imgBarCode1 = document.querySelector("img.barCode1");
-      const imgBarCode2 = document.querySelector("img.barCode2");
-      const imagePath1 = imgBarCode1.getAttribute("src");
-      const imagePath2 = imgBarCode2.getAttribute("src");
-
-      // File Does Not exists:
-      const barCode1 = await BarcodeScanner.readBarcodesFromImage({path: imagePath1, formats: 'CODE_128'})
-      const barCode2 = await BarcodeScanner.readBarcodesFromImage({path: imagePath2, formats: 'CODE_128'})
-
-      console.assert(barCode1 === '50912735')
-      console.assert(barCode2 === '61795020')
+      document.getElementById('open-image').addEventListener('click', async () => {
+        await this.readBarcodeFromImage()
+      })
     }
   },
 );
